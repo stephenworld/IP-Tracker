5d9fec8c64a80bec22377700aaeff4564d8e9bde
Adewale Stephen <adewalestephennifemi@gmail.com>
2025-07-02 21:39:19 +0100

    dynamically update IPInfo Component

--- src/Components/IPInfo.jsx
+++ src/Components/IPInfo.jsx
@@ -1,8 +1,8 @@
 export default function IPInfo({ title, content }) {
   return (
-    <div className="w-full">
-      <h2>{title}</h2>
-      <p>{content}</p>
+    <div className="w-full text-center md:text-left">
+      <p className="uppercase text-xs text-gray-500">{title}</p>
+      <h2 className="font-bold">{content}</h2>
     </div>
   );
 }
