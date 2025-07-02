b8501c010ad24647ba295c9fc78f5503cb4d9648
Adewale Stephen <adewalestephennifemi@gmail.com>
2025-07-02 21:36:12 +0100

    Moved functons to App Component

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,7 +1,8 @@
 import arrow from "../assets/images/icon-arrow.svg";
-export default function Form() {
+export default function Form({ handleSubmit, handleInput, input }) {
   return (
     <form
+      onSubmit={handleSubmit}
       action="#"
       className="w-full flex items-center rounded-md overflow-hidden"
     >
@@ -9,10 +10,12 @@ export default function Form() {
         className="w-full text-sm px-4 py-2 h-12 bg-white border-none outline-none placeholder:text-gray-900"
         type="text"
         placeholder="Search for an IP address or domain"
+        value={input}
+        onChange={handleInput}
       />
       <button
         type="submit"
-        className="bg-gray-900 p-2 h-12 w-14 flex justify-center items-center"
+        className="cursor-pointer bg-gray-900 p-2 h-12 w-14 flex justify-center items-center"
       >
         <img src={arrow} alt="arrow" />
       </button>
