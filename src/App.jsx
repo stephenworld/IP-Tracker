8ffb61f4f52ea39ac2c60c0f05474dd5c3d3ab87
Adewale Stephen <adewalestephennifemi@gmail.com>
2025-07-02 21:35:26 +0100

    moved form functions and data to App component for better data transmission

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,42 @@
+import { useState } from "react";
 import Form from "./Components/Form";
 import IPDetail from "./Components/IPDetail";
 import Map from "./Components/Map";
 import desktopBackground from "./assets/images/pattern-bg-desktop.png";
 import mobileBackground from "./assets/images/pattern-bg-mobile.png";
+const isMobile = window.innerWidth <= 768;
+const bgImage = isMobile ? mobileBackground : desktopBackground;
+const dummyData = {
+  ip: "8.8.8.8",
+  location: {
+    country: "US",
+    region: "California",
+    timezone: "-07:00",
+    lat: 37.386,
+    lng: -122.0838,
+    city: "Mountain View",
+  },
+  isp: "Google LLC",
+};
 export default function App() {
-  const isMobile = window.innerWidth <= 768;
-  const bgImage = isMobile ? mobileBackground : desktopBackground;
+  const [formData, setFormData] = useState(dummyData);
+  const [input, setInput] = useState("");
+  function handleSubmit(e) {
+    e.preventDefault();
+    const API_KEY = "at_Exzd1IFm0dzpSdXFFPwQNr71KR1up";
+    const url = `https://geo.ipify.org/api/v2/country,city?apiKey=${API_KEY}&ipAddress=${input}`;
+
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setFormData(data));
+
+    console.log(formData);
+  }
   return (
     <main className="h-screen relative">
       <section
         style={{ backgroundImage: `url(${bgImage})` }}
-        className="relative bg-cover bg-no-repeat px-[5%] md:px-16 py-8 pb-16 h-3/10"
+        className="bg-cover bg-center bg-no-repeat px-[5%] md:px-16 py-8 pb-16 h-3/10"
       >
         <div className="w-full md:w-1/2 m-auto">
           <header className="text-center mb-4">
@@ -18,11 +44,20 @@ export default function App() {
               IP Address Tracker
             </h1>
           </header>
-          <Form />
+          <Form
+            handleSubmit={handleSubmit}
+            input={input}
+            handleInput={(e) => setInput(e.target.value)}
+          />
         </div>
       </section>
-      <IPDetail />
-      <Map />
+      <Map
+        position={[
+          formData?.location?.lat || 6.5244,
+          formData?.location?.lng || 3.3792,
+        ]}
+      />
+      <IPDetail data={formData} />
     </main>
   );
 }
