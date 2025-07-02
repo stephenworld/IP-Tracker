e13becb99e9dbb0d4e778941dd4457f150beb8e6
Adewale Stephen <adewalestephennifemi@gmail.com>
2025-07-02 21:38:20 +0100

    get location dynamically and implementatio of map API from react leaflet

--- src/Components/Map.jsx
+++ src/Components/Map.jsx
@@ -1,23 +1,28 @@
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
-export default function Map() {
-  const position = [51.505, -0.09];
+import { useEffect } from "react";
+
+function RecenterMap({ position }) {
+  const map = useMap();
+  useEffect(() => {
+    map.setView(position);
+  }, [position, map]);
+  return null;
+}
+
+export default function Map({ position }) {
   return (
-    <section className="h-7/10 w-full">
-      <section
-        className="w-full h-full"
-        style={{ height: "70svh", width: "100%" }}
-      >
-        <MapContainer center={position} zoom={13} className="h-full w-full -z-10">
-          <TileLayer
-            attribution='&copy; <a href="https://osm.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-          <Marker position={position}>
-            <Popup>Here</Popup>
-          </Marker>
-        </MapContainer>
-      </section>
+    <section className="w-full h-[70svh]">
+      <MapContainer center={position} zoom={12} className="h-full w-full">
+        <RecenterMap position={position} />
+        <TileLayer
+          attribution='&copy; <a href="https://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={position}>
+          <Popup>Your Location</Popup>
+        </Marker>
+      </MapContainer>
     </section>
   );
 }
